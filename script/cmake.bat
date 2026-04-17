655a828a63a9cf89e9feaaca15a5c623c711aa08
petokobu <peto.kobu@gmail.com>
2026-04-17 17:52:24 +0200

    make script positional argument fix & printing

--- script/cmake.bat
+++ script/cmake.bat
@@ -1,10 +1,12 @@
-@ECHO OFF
+@echo off
 
 set exec=cset_T0
 set main=main
 g++ "%~dp0..\src\%main%.c" "%~dp0..\src\cset.cpp" "%~dp0..\src\set.cpp" "%~dp0..\src/rbtree.cpp" "%~dp0..\src\llist.cpp" "%~dp0..\src\htable.cpp" -o "%~dp0..\bin\%exec%"
-IF "%1"=="-r" (
+if "%1"=="-r" (
     "%~dp0..\bin\%exec%"
 )
 
-ECHO ON
+echo "Compiled src/%main%.c into bin/%exec%.exe"
+
+echo on
--- script/make.bat
+++ script/make.bat
@@ -1,14 +1,20 @@
-@ECHO OFF
+@echo off
 
 set exec=set
 set main=main
-IF "%2"=="-t" (
+if "%2"=="-t" (
+    set exec=set_T0
+    set main=testing
+)
+if "%1"=="-t" (
     set exec=set_T0
     set main=testing
 )
 g++ "%~dp0..\src\%main%.cpp" "%~dp0..\src\set.cpp" "%~dp0..\src/rbtree.cpp" "%~dp0..\src\llist.cpp" "%~dp0..\src\htable.cpp" -o "%~dp0..\bin\%exec%"
-IF "%1"=="-r" (
+if "%1"=="-r" (
     "%~dp0..\bin\%exec%"
 )
 
-ECHO ON
+echo "Compiled src/%main%.cpp into bin/%exec%.exe"
+
+echo on
